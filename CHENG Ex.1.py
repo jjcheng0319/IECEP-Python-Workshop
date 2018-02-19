37faff7c5b64ed964825bc52d89c3db24c4e15ec
John Cheng <36612259+jjcheng0319@users.noreply.github.com>
2018-02-19 12:58:32 +0800

    Add files via upload

---
+++ CHENG Ex.1.py
@@ -0,0 +1,7 @@
+balance=input("What is your current balance? ")
+iphonex=50000
+print "Your current balance is", balance
+total=balance/iphonex
+print "You can buy" , total, " 'iPhone X's "
+total1=balance-iphonex*total
+print "You have", total1, "money left after buying" , total,"'iPhone X's"
---
+++ CHENG Ex.2.py
@@ -0,0 +1,5 @@
+x = input("Input an integer: ")
+intgr=1
+for x in range(1,x+1):
+    intgr=intgr*x
+print "The factorial of the integer is" , intgr
